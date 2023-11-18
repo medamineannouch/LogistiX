fca1ebb33b5b323bce7820d1e5e760848bcb927d
medamineannouch <mohamedamine.annouch@gmail.com>
2023-11-18 17:41:59 +0100

    add more tests

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import instance
-
+import time
+import pre_clusterer
 
 class TestInstances(unittest.TestCase):
     def test_location_sample(self):
@@ -19,15 +20,30 @@ class TestInstances(unittest.TestCase):
                         break
 
     def test_instance_generation(self):
-        for (weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.instances():
+        for (weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
             for dic in [cust, plnt, dc]:
                 nout = 0
+
                 for i in dic:
                     print(i, dic[i], name[i])
                     nout += 1
                     if nout >= 3:
                         print("...\n")
                         break
+
+
+    def test_pre_clusterer(self):
+        """
+        Test optimizing the location of a small number of dc's from set of candidates
+        """
+        for (weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
+            start = time.process_time()
+            prods = weight.keys()
+            n_clusters = (10 + len(dc)) // 5
+            cluster_dc = pre_clusterer.preclustering(cust, dc, prods, demand, n_clusters)
+            end = time.process_time()
+            print("Clustered dc's, used {} seconds".format(end - start))
+            print("Selected", len(cluster_dc), "dc's out of", len(dc.keys()), "possible positions")
 
 if __name__ == '__main__':
     unittest.main()
