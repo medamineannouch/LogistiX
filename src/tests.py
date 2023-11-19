5d8f192892e378eb8a28fec73668fe932e66cbbf
medamineannouch <mohamedamine.annouch@gmail.com>
2023-11-19 18:12:20 +0100

    update

--- src/tests.py
+++ src/tests.py
@@ -2,6 +2,9 @@ import unittest
 import instance
 import time
 import pre_clusterer
+from geopy.distance import great_circle as distance
+from gurobipy import Model, GRB
+from model import multiple_src, single_src, mk_costs
 
 class TestInstances(unittest.TestCase):
     def test_location_sample(self):
@@ -20,7 +23,7 @@ class TestInstances(unittest.TestCase):
                         break
 
     def test_instance_generation(self):
-        for (weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
+        for (prods,weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
             for dic in [cust, plnt, dc]:
                 nout = 0
 
@@ -36,7 +39,7 @@ class TestInstances(unittest.TestCase):
         """
         Test optimizing the location of a small number of dc's from set of candidates
         """
-        for (weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
+        for (prods,weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
             start = time.process_time()
             prods = weight.keys()
             n_clusters = (10 + len(dc)) // 5
@@ -44,6 +47,82 @@ class TestInstances(unittest.TestCase):
             end = time.process_time()
             print("Clustered dc's, used {} seconds".format(end - start))
             print("Selected", len(cluster_dc), "dc's out of", len(dc.keys()), "possible positions")
+
+
+        """
+        Unit tests for the solving functions in lnd_ms and lnd_ss.
+        """
+
+    def setUp(self):
+        """
+        Set up any common variables or configurations needed for the tests.
+        """
+        self.TIME_LIMIT = 300  # Allow Gurobi to use 5 minutes
+
+    def test_optimization_with_small_instance(self):
+        """
+        Test optimizing the location of a small number of dc's from a set of candidates.
+        """
+        models = {
+            "multiple source": multiple_src,
+            "single source": single_src
+        }
+
+        for k in models:
+            for (prods,weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name) in instance.mk_instances():
+                print(f"* using {k} model *")
+                print(f"*** new instance, {len(plnt)} plants + {len(dc)} dc's + {len(cust)} customers ***")
+                start = time.process_time()
+                (tp_cost, del_cost, dc_fc, dc_vc) = mk_costs(plnt, dc, cust)
+                dc_num = (20 + len(dc)) // 10
+
+                model = models[k](prods,weight, cust, dc, dc_ub, plnt, plnt_ub, demand, tp_cost, del_cost, dc_fc, dc_vc, dc_num)
+                model.setParam('TimeLimit', self.TIME_LIMIT)
+                model.optimize()
+
+                EPS = 1.e-6
+                for x in model.getVars():
+                    if x.X > EPS:
+                        print(x.varName, x.X)
+
+                end = time.process_time()
+                print(f"solving MIP used {end - start} seconds")
+                print()
+
+
+
+
+    def test_demand_keys(self):
+
+        for inst in instance.mk_instances():
+            prods, weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name = inst
+
+            # Ensure that all keys in the 'demand' dictionary have corresponding entries in 'cust' and 'prods'
+            for (customer, product) in demand.keys():
+                self.assertIn(customer, cust, f"Customer {customer} not found in 'cust' dictionary")
+                self.assertIn(product, prods, f"Product {product} not found in 'prods' list")
+
+            # Ensure that all customers in 'cust' have corresponding entries in 'demand'
+            for customer in cust.keys():
+                for product in prods:
+                    self.assertIn((customer, product), demand, f"Missing entry for ({customer}, {product}) in 'demand'")
+
+    def test_constraints_indices(self):
+        insts = instance.mk_instances()
+
+        for inst in insts:
+            prods, weight, cust, plnt, dc, dc_lb, dc_ub, demand, plnt_ub, name = inst
+            print("prods:", prods)
+            print("weight:", weight)
+            print("cust:", cust)
+            print("demand:", demand)
+            model = multiple_src(prods, weight, cust, dc, dc_ub, plnt, plnt_ub, demand, {}, {}, {}, {}, 1)
+
+            # Check the indices in the constraints
+            for constr in model.getConstrs():
+                constr_str = str(constr)
+                self.assertNotIn("None", constr_str, f"Constraint {constr} contains 'None' in its indices.")
+
 
 if __name__ == '__main__':
     unittest.main()
